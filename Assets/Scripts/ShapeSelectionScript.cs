295a078c1a2322e53f51f5388793f6d5a68a6db8
barcowivan <wim.vandamme@barco.com>
2016-10-22 23:03:57 +0200

    changes

--- Assets/Scripts/ShapeSelectionScript.cs
+++ Assets/Scripts/ShapeSelectionScript.cs
@@ -4,7 +4,7 @@ using WorkshopVR;
 
 public class ShapeSelectionScript : MonoBehaviour {
 
-    public float DocumentDistanceToUserWhenDragged = 20;
+    public float DocumentDistanceToUserWhenDragged = 1.1f;
     private Document selectedDocument;
     private ViveInput viveInput;
     private bool isEntered = false;
@@ -49,12 +49,34 @@ public class ShapeSelectionScript : MonoBehaviour {
 
     private void AllowToMove()
     {
-        selectedDocument.transform.parent = transform; 
-        Vector3 fromDocToPointer = transform.position - selectedDocument.transform.position;
-        fromDocToPointer.Normalize();
-        selectedDocument.transform.Translate(fromDocToPointer);
-    }
+        Transform anchor = transform.FindChild("AnchorHolding");
+        selectedDocument.transform.parent = anchor;
 
+       // selectedDocument.transform.localPosition = transform.position ;
+        //Vector3 fromDocToPointer = transform.position - selectedDocument.transform.position;
+        //float percentageDifferenceAllowed = 0.01f; // is 1%
+
+        //if (!Approximately(transform.position - selectedDocument.transform.position, fromDocToPointer*DocumentDistanceToUserWhenDragged, percentageDifferenceAllowed))
+        //{
+        //    fromDocToPointer.Normalize();
+        //    selectedDocument.transform.Translate(fromDocToPointer);
+        //}
+
+    }
+    public bool Approximately(Vector3 me, Vector3 other, float allowedDifference)
+    {
+        var dx = me.x - other.x;
+        if (Mathf.Abs(dx) > allowedDifference)
+            return false;
+
+        var dy = me.y - other.y;
+        if (Mathf.Abs(dy) > allowedDifference)
+            return false;
+
+        var dz = me.z - other.z;
+
+        return Mathf.Abs(dz) >= allowedDifference;
+    }
     public Document GetSelectedDocument()
     {
         return selectedDocument;
