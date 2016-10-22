afdf817450eff32decd2cda145a75e1a28cf3373
barcowivan <wim.vandamme@barco.com>
2016-10-23 00:49:32 +0200

    sphere collision stop

---
+++ Assets/Scripts/SphereCollision.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereCollision : MonoBehaviour {
+
+    void Start()
+    {
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        collision.rigidbody.velocity = Vector3.zero;
+        collision.rigidbody.angularVelocity = Vector3.zero; ;
+
+        Invoke("ResetKimematic", 2);
+
+    }
+    void ResetKimematic()
+    {
+        //collision.rigidbody.isKinematic = true;
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
