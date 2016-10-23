d59a8649f3429a8ab833255d359d234437796fe5
Colin-b <colin.bounouar@gmail.com>
2016-10-23 06:35:16 +0200

    Add the ability to play video files thanks to Movie Texture

---
+++ Assets/Scripts/VideoDocument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class VideoDocument : Document
+{
+
+    public override void Open()
+    {
+        MovieTexture movie = (MovieTexture)GetComponent<Renderer>().material.mainTexture;
+        if (movie.isPlaying)
+            movie.Stop();
+        else
+            movie.Play();
+    }
+}
