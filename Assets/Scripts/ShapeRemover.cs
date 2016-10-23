7e55ce5f618e520a12eb5a3dd50033eb412b9f22
Colin-b <colin.bounouar@gmail.com>
2016-10-23 10:16:58 +0200

    Retrieve proper document color after removal Add new sounds

--- Assets/Scripts/ShapeRemover.cs
+++ Assets/Scripts/ShapeRemover.cs
@@ -3,26 +3,28 @@ using System.Collections;
 using WorkshopVR;
 
 public class ShapeRemover : MonoBehaviour {
+
     private ViveInput2 viveInput;
     private Document selectedDocument;
-    // Use this for initialization
+
     void Start () {
         SteamVR_LaserPointer lazer = GetComponent<SteamVR_LaserPointer>();
-        //lazer.PointerOut += LeaveObject;
         lazer.PointerIn += EnterObject;
 
         viveInput = GetComponent<ViveInput2>();
     }
+
     private void EnterObject(object sender, PointerEventArgs e)
     {
         if (selectedDocument == null)
             selectedDocument = e.target.gameObject.GetComponent<Document>();
     }
-    // Update is called once per frame
+
     void Update () {
         if (selectedDocument != null && !selectedDocument.IsSelected && viveInput.IsTriggerPressed())
             RemoveDocument();
     }
+
     private void RemoveDocument()
     {
         Rigidbody body = selectedDocument.GetComponent<Rigidbody>();
@@ -31,12 +33,12 @@ public class ShapeRemover : MonoBehaviour {
         selectedDocument.SelectedRemove();
         ThrowToGarbageZone();
     }
+
     private void ThrowToGarbageZone()
     {
         Rigidbody body = selectedDocument.GetComponent<Rigidbody>();
         body.AddForce(new Vector3(0,0,-70), ForceMode.Impulse);
         selectedDocument.UnSelectedRemove();
         selectedDocument = null;
-
     }
 }
